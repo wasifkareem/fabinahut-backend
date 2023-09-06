73e399174475a5ce688d85671729a07158f1758b
wasif kareem <wasifkareem2762@gmail.com>
2023-09-07 02:13:34 +0530

    server modified

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,10 +44,10 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-    if (!user) return res.status(400).json({ msg: "user does not exist" });
+    if (!user) return res.status(400).json(null);
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+    if (!isMatch) return res.status(400).json(null);
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
